613f2efe04bb3055756cd8f8d1f81c8188b7cb03
bryanttsang <43279760+bryanttsang@users.noreply.github.com>
2019-03-01 14:36:07 -0500

    Update DeckTester.java

--- src/DeckTester.java
+++ src/DeckTester.java
@@ -10,7 +10,7 @@ public class DeckTester {
     public static void main(String[] args) {
         String[] ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
         String[] suits = {"♠", "♥", "♣", "♦"};
-        int[] pointValues = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+        int[] pointValues = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 0, 0};
         Deck d = new Deck(ranks, suits, pointValues);
         d.shuffle();
 
